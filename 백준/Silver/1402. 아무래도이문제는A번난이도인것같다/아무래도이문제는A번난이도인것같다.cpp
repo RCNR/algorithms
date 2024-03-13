46671ffa15ea52f734d49044d81e2480e085ce63
RCNR <160254953+RCNR@users.noreply.github.com>
2024-03-13 21:32:46 +0900

    Create 아무래도이문제는A번난이도인것같다.cpp

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define rep(i, a, n) for(auto i = a; i < n; i++)
+#define REP(i, a, n) for(auto i = a; i <= n; i++)
+#define pb push_back
+#define endl "\n"
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0)
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int, int>
+#define X first
+#define Y second
+#define all(v) (v).begin(), (v).end()
+#define PIV (1 << 20)
+
+using namespace std;
+
+int n;
+
+
+int main(void) {
+    FAST_IO;
+
+    cin >> n;
+    while (n--) {
+        int a, b;
+        cin >> a >> b;
+        cout << "yes" << endl;
+    }
+
+}
