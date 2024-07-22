75114f6ce3fdf69bf3f80746502fab6eb8f443a5
RCNR <iop04226@gmail.com>
2024-07-22 23:36:40 +0900

    87%에서 틀렸습니다. 코드

--- Baekjoon/2000/공주님의 정원.py
+++ Baekjoon/2000/공주님의 정원.py
@@ -13,17 +13,36 @@ for _ in range(n):
 
 board.sort()
 
-d_day = 0 # 꽃 지는 날
-long_flower = 301 # 날짜 비교해야함
+end_day = 301 # 꽃 지는 날
+# 날짜 비교해야함
 
 cnt = 0
+flower_period = 0
+flower_start = 301
 
 while board:
-    if d_day >= 1201 or long_flower < board[0][0]: break
+    if end_day >= 1201 or end_day < board[0][0]: break
 
     for _ in range(len(board)):
-        if board[0][0] <= long_flower:
-            d_day = board[0][1]
+        if board[0][0] <= flower_start:
+            period = board[0][1] - flower_start
+            if period > flower_period: # 얘가 더 좋은 꽃인거임
+                flower_period = period
+                end_day = board[0][1]
+            #print(board[0])
+            board.remove(board[0])
+        else: break # flower_start 값과 비교했을 때 더 이상 볼 필요가 없는 경우
+    #print(end_day)
+    #print()
+    cnt += 1
+    flower_start = end_day
+    flower_period = 0 # 이 기간을 초기화시켜줘야 다음 꽃 기간을 선정할 때 영향을 주지 않음 -> 여기서 시간 이빠이 잡아먹음
+
+#print(cnt)
+
+if len(board) and end_day < board[0][0]:
+    print(0)
+    exit(0)
+
+print(cnt)
     
-
-
