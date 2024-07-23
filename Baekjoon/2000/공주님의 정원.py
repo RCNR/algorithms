c2b2d72f7747ea9b6cac9b8102d531b34ffbcc23
RCNR <iop04226@gmail.com>
2024-07-23 13:25:24 +0900

    공주님의 정원.py - update

--- Baekjoon/2000/공주님의 정원.py
+++ Baekjoon/2000/공주님의 정원.py
@@ -29,25 +29,21 @@ while board:
             if period > flower_period: # 얘가 더 좋은 꽃인거임
                 flower_period = period
                 end_day = board[0][1]
-            #print(board[0])
             board.remove(board[0])
         else: break # flower_start 값과 비교했을 때 더 이상 볼 필요가 없는 경우
-    #print(end_day)
-    #print()
     cnt += 1
     flower_start = end_day
     flower_period = 0 # 이 기간을 초기화시켜줘야 다음 꽃 기간을 선정할 때 영향을 주지 않음 -> 여기서 시간 이빠이 잡아먹음
-
-#print(cnt)
 
 if len(board) and end_day < board[0][0]:
     print(0)
     exit(0)
 
-if end_day == 1130:
+if end_day <= 1130:
     print(0)
     exit(0)
 
 print(cnt)
 
 # 87%에서의 반례 발견 : 3 1 11 30 -> 답 : 0, 출력 : 1
+# 98%에서의 틀렸습니다 : end_day가 <= 1130 임을 고려하지 않았었음
