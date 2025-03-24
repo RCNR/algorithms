e9490044c3e0267b74846820c35da37cc9847b9c
RCNR <iop04226@gmail.com>
2025-03-25 01:01:21 +0900

    Create 거스름돈.java

---
+++ Baekjoon/14000/거스름돈.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+
+        while (n >= 0) {
+            if (n % 5 == 0) {
+                cnt += n / 5;
+                bw.write(cnt +"");
+                bw.flush();
+                bw.close();
+
+                System.exit(0);
+            } else {
+                n -= 2;
+                cnt += 1;
+            }
+        }
+        bw.write(String.valueOf(-1));
+
+        bw.flush();
+        bw.close();
+    }
+}
